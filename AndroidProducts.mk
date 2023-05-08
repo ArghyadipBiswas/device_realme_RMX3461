ef7f3f1be8617c828c0c842552fb2c1e622ca209
ZirgomHaidar <zirgomhaidar17@gmail.com>
2023-05-08 12:33:35 +0530

    Welcome lineageOS :)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The superiorOS Project
+# Copyright (C) 2021 The lineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_RMX3461.mk
+    $(LOCAL_DIR)/lineage_RMX3461.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_RMX3461-user \
-    superior_RMX3461-userdebug \
-    superior_RMX3461-eng
+    lineage_RMX3461-user \
+    lineage_RMX3461-userdebug \
+    lineage_RMX3461-eng
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,30 +1,20 @@
-#!/bin/bash
-
-banner(){
-clear
-echo "=================================="
-echo "|                                |"
-echo "|   Realme 9 5G SE Setup Script  |"
-echo "|       Branch: Superior         |"
-echo "|  Maintained By: ZirgomHaidar   |"
-echo "|                                |"
-echo "=================================="
-}
-
-banner 2>1
-
 #Clone vibrator HAL
-banner 2>1
-rm -rf vendor/qcom/opensource/vibrator
+#rm -rf vendor/qcom/opensource/vibrator
 git clone https://github.com/itsxrp/android_vendor_qcom_opensource_vibrator.git vendor/qcom/opensource/vibrator
 
 # Clone Display Hals
-rm -rf hardware/qcom-caf/sm8350/display
+#rm -rf hardware/qcom-caf/sm8350/display
 git clone https://github.com/Cykeek-Labs/hardware_qcom-caf_sm8350_display.git hardware/qcom-caf/sm8350/display
 
 # Clone livedisplay
 git clone https://github.com/LineageOS/android_hardware_lineage_livedisplay hardware/lineage/livedisplay
 
 # Clone Bluetooth
-rm -rf packages/modules/Bluetooth
+#rm -rf packages/modules/Bluetooth
 git clone https://github.com/ZirgomHaidar/android_packages_modules_Bluetooth packages/modules/Bluetooth
+
+# Clone kernel
+git clone https://github.com/ZirgomHaidar/kernel_realme_sm8350 kernel/realme/sm8350
+
+# Clone hardware
+git clone https://github.com/LineageOS/android_hardware_oplus hardware/oplus
